f1b328643df788e1f9f27fa496e5a3f67c77b6db
Ritchel M <ritchiemesidor84@gmail.com>
2024-09-21 20:57:05 -0400

    Ritchie's code from Bianca's lesson

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (name = "Auto Code")
+public class Auto extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    Hardware robot = Hardware.getInstance();
+
+
+    public void runOpMode(){
+        robot.init(hardwareMap);
+        waitForStart();
+        //move 10 inches
+        move(10, 1);
+        //turns
+        turn(1000, 1);
+        //
+        timeMove()
+    }
+
+    //create method to move robot forwards or backwards a certain distance and speed
+    public void move(double distance, double speed){
+
+        //converts ticks to inches
+        double wheelCircumference = 4*Math.PI;
+        double wheelRPM = 560;
+        double ticks = (distance * (wheelRPM/wheelCircumference));
+
+        //set power to 0
+        robot.setPower(0, 0, 0, 0);
+
+        //set target position
+        robot.rf.setTargetPosition((int)Math.round(ticks));
+        robot.rb.setTargetPosition((int)Math.round(ticks));
+        robot.lf.setTargetPosition((int)Math.round(ticks));
+        robot.lb.setTargetPosition((int)Math.round(ticks));
+
+        //reset encoder to get rid of old value
+        robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set run to position
+        robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //setting power
+        robot.setPower(speed, speed,speed, speed);
+        //keep robot going to target position
+        while (opModeIsActive() && (robot.rf.isBusy())){
+
+        }
+        //set power to 0
+        robot.setPower(0, 0,0, 0);
+    }
+    //method to turn
+     public void turn(int ticks, double speed){
+         robot.rf.setTargetPosition(ticks);
+         robot.rb.setTargetPosition(ticks);
+         robot.lf.setTargetPosition(ticks);
+         robot.lb.setTargetPosition(ticks);
+
+         robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+         robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+         robot.setPower(-speed, -speed,speed, speed);
+
+         while (opModeIsActive() && (robot.rf.isBusy())){
+
+         }
+         robot.setPower(0, 0,0, 0);
+         robot.rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+     }
+     public void timeMovement(double speed, double time){
+            runtime.reset();
+         robot.rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         robot.rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         robot.lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         robot.lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+         robot.setPower(-speed, speed, -speed, speed);
+
+         while (runtime.seconds() < time){
+
+         }
+
+         robot.setPower(0, 0,0, 0);
+     }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+public class Hardware {
+    public DcMotor rf;
+    public DcMotor rb;
+    public DcMotor lf;
+    public DcMotor lb;
+    public Servo demoServo;
+
+    public static double maxSpeed = 0.5;
+    private static Hardware instance = null;
+    public static Hardware getInstance() {
+        if(instance == null){
+            instance = new Hardware();
+        }
+        return instance;
+    }
+    public void init(HardwareMap hwMap){
+        rf = hwMap.get(DcMotor.class, "demoWheel1");
+        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setPower(0);
+
+        rb = hwMap.get(DcMotor.class, "demoWheel2");
+        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setPower(0);
+
+        lf = hwMap.get(DcMotor.class, "demoWheel3");
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setPower(0);
+
+        lb = hwMap.get(DcMotor.class, "demoWheel4");
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setPower(0);
+        demoServo = hwMap.get(Servo.class, "demoServo");
+
+    }
+
+    public void setPower(double fr, double br, double fl, double bl){
+        rf.setPower(Range.clip(fr, -maxSpeed, maxSpeed));
+        rb.setPower(Range.clip(br, -maxSpeed, maxSpeed));
+        lf.setPower(Range.clip(fl, -maxSpeed, maxSpeed));
+        lb.setPower(Range.clip(bl, -maxSpeed, maxSpeed));
+    }
+}
